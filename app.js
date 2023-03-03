b4b182da2577ee61dda117b66343ce0f1218d34f
Ahamad-Tanzeel-Khan <117995820+Ahamad-Tanzeel-Khan@users.noreply.github.com>
2023-03-03 06:51:20 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -18,19 +18,77 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static("public"));
 
+app.use(session({
+  secret: "My little secret.",
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 mongoose.set('strictQuery', false);
 
 mongoose.connect("mongodb://127.0.0.1:27017/userDB", {useNewUrlParser: true});
 
 const userSchema = new mongoose.Schema ({
-  username:String,
   email: String,
   password: String,
   googleId: String,
 });
 
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
+
 const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+// For my Google strategy 
+passport.serializeUser(function(user, done) {
+  process.nextTick(function() {
+    done(null, { id: user.id, username: user.username, name: user.name });
+  });
+});
+
+passport.deserializeUser(function(user, done) {
+  process.nextTick(function() {
+    return done(null, user);
+  });
+});
+
+passport.use('google',
+new GoogleStrategy(
+{
+  clientID: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  callbackURL: "http://localhost:3000/auth/google/test",
+  passReqToCallback:true
+},
+async (accessToken, refreshToken, profile, done) => {
+  console.log(profile)
+  const existingUser = await User.findOne({ googleId: profile.id });
+
+  if (existingUser) { 
+    done(null, existingUser);
+  } else {
+    const user = await new User({ googleId: profile.id, token: accessToken, name:profile.displayName }).save();
+    done(null, user);
+    }
+  }
+ )
+);
+
+app.get('/auth/google', 
+  passport.authenticate("google", {scope: [ "profile"]}
+));
+
+app.get('/auth/google/test',
+  passport.authenticate('google', { failureRedirect: "/login", failureMessage: true }),
+  function(req, res) {
+    res.redirect("/test" );
+});
+
 
 app.get("/", function(req,res){
   res.render("home");
@@ -45,25 +103,64 @@ app.get("/login", function(req,res){
 });
 
 app.get("/test", function(req,res){
-  res.render("test");
+  if(req.isAuthenticated()){
+    res.render("test");
+  } else {
+    res.redirect("/login");
+  }
 });
 
 app.post("/register", function(req,res){
-  const newUser = new User({
-    email: req.body.username,
-    password: req.body.password 
+  const username = req.body.username;
+  const password = req.body.password;
+
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password
+  });
+  User.register({email: req.body.username}, req.body.password, function(err,user){
+    if(err){
+      console.log(err);
+      res.redirect("/register");
+    } else {
+      passport.authenticate("local")(req,res, function(){
+        res.redirect("/test");
+      });
+    }
   });
 
-  newUser.save(function(err){
+});
+
+app.post("/login", function(req,res){
+  const username = req.body.username;
+  const password = req.body.password;
+
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password
+  });
+
+  req.login(user, function(err){
     if(err){
       console.log(err);
     } else {
+      passport.authenticate("local");
       res.redirect("/test");
     }
   })
+
+  User.findOne({email:username})
+    .then((foundUser) => {
+        if(foundUser){
+            if(foundUser.password === password){
+                res.redirect("test");
+            }
+            else{
+              res.redirect("/register");
+            }
+        }
+   })
 });
-
-
 
 
 app.listen(3000, function() {
